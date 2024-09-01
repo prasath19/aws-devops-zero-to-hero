631aa4e1ed53da93b511ccb0113c8cd96d15f248
prasath19 <88819428+prasath19@users.noreply.github.com>
2024-09-02 00:31:43 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,10 +4,14 @@ set -e
 # Stop the running container (if any)
 containerid=$(docker ps -q)
 
+# Debugging output
+echo "Container ID(s): $containerid"
+
 # Check if a container ID was found
 if [ -n "$containerid" ]; then
+    echo "Stopping and removing container(s) with ID(s): $containerid"
     docker rm -f $containerid
-    echo "Previous container deleted"
+    echo "Previous container(s) deleted"
 else
     echo "No running container found"
 fi
